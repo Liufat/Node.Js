127cf8e0e1f9a22111418ce0c2d1c95b0f08908e
Liufat <dd5672003@gmail.com>
2022-10-07 11:39:17 +0800

    新增/修改

---
+++ 1007/01/http-server02.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs').promises;
+const server = http.createServer((request, response) => {
+    fs.writeFile(__dirname + '/headers.txt', JSON.stringify(request.headers))
+        .then(data => {
+            console.log({ data });
+            response.writeHead(200, {
+                'content-type': 'text/html'
+            });
+            response.end(`<h2>${request.url}</h2>`);
+
+        })
+
+
+})
+
+server.listen(3000);
