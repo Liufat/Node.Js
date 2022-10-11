0196d0d872184e7f1725f988b98e0346834a8951
Liufat <dd5672003@gmail.com>
2022-10-11 10:51:44 +0800

    EJS

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const express = require('express');
 const app = express();
 //建立路由
 
+app.set('view engine', 'ejs')//set要放在最前面
+app.set('views','1011-views')
 
 app.get('/', (req, res) => {
-    res.send(`<h2>你好</h2>`);
+    // res.send(`<h2>你好</h2>`);
+    res.render('main', { name: 'Shinder' });
+              //樣板名    //要傳的變數
 })
 app.use(express.static('1011-public'))
 app.use(express.static('node_modules/bootstrap/dist'))
 //C:\Users\劉肥\OneDrive\前端資料\07.NodeJS\mfee29-node\node_modules\bootstrap\dist\css\bootstrap-reboot.rtl.css.map
-app.use((req,res)=>{
+app.use((req, res) => {
     // res.type('text/plain');//純文字，如果沒設定預設是type:html
     res.status(404).send('<h1>找不到頁面</h1>');
 })
@@ -21,6 +25,6 @@ app.use((req,res)=>{
 
 const port = process.env.SEVER_PORT || 3002;
 //如果沒有設定前者，則使用3002通訊port
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server started, port: ${port}`);
 });
