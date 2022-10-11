e520882034422c904b3501a92f6ad10dc7251222
Liufat <dd5672003@gmail.com>
2022-10-11 09:41:30 +0800

    設定port與404頁面

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 //引入express
 const express = require('express');
 //建立物件
@@ -7,7 +9,14 @@ app.get('/', (req, res) => {
     res.send(`<h2>你好</h2>`);
 })
 
+app.use((req,res)=>{
+    // res.type('text/plain');//純文字，如果沒設定預設是type:html
+    res.status(404).send('<h1>找不到頁面</h1>');
+})
 
-app.listen(3000, ()=>{
-    console.log('server started');
+
+const port = process.env.SEVER_PORT || 3002;
+//如果沒有設定前者，則使用3002通訊port
+app.listen(port, ()=>{
+    console.log(`server started, port: ${port}`);
 });
