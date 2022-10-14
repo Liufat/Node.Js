379f298596cbd4c8eda857aff457350cb113c916
Liufat <dd5672003@gmail.com>
2022-10-14 13:40:45 +0800

    更改資料庫路徑匯入方式(env)

--- modules/db_connect2.js
+++ modules/db_connect2.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 //建立連線池(效能比較好)
 const pool = mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    password:'root',
-    database:'project57',
+    host: process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
     waitForConnections:true,//是否等待連線完成？
     connectionLimit:10,//同時最大連線數
     queueLimit:0//0 = 不限制排隊數量
